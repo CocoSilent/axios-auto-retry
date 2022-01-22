8d5bacf5223d35ed46428dd9efcfe19a0d730d23
xsl <xsl@servyou.com.cn>
2022-01-22 19:35:00 +0800

    代码优化

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,47 @@
 import { RetryConfig, RetryState } from './type';
 import { AxiosStatic, AxiosInstance, AxiosResponse } from 'axios';
-import { sleep } from './util'
+import { sleep } from './util';
+
+
+function whetherRetry(retryConfig: RetryConfig, retryState: RetryState, response: AxiosResponse) {
+    const { retryCount, retryCondition } = retryConfig;
+
+    let retry = false;
+    let currentRetryCount = retryState?.count || 0;
+    if ( currentRetryCount < retryCount) {
+        retry = retryCondition(response);
+    }
+    return retry;
+}
+
+function getDelay(retryConfig: RetryConfig, retryState: RetryState) {
+    let delay = 0;
+    const retryStateCount = retryState.count || 0;
+    if (retryConfig.retryDelay) {
+        delay = retryConfig.retryDelay(retryStateCount + 1);
+    }
+    return delay;
+}
 
 function axiosAutoRetry(instance: AxiosStatic | AxiosInstance , retryConfig: RetryConfig) {
-    (instance.interceptors.response as any).handlers.unshift({
-        fulfilled: (response: AxiosResponse & { config: { retryConfig: RetryConfig, retryState: RetryState } }) => {
-            const config = Object.assign(retryConfig, response.config.retryConfig);
-            const retryState = response.config.retryState;
-            const { retryCount, retryCondition, retryDelay } = config;
+    // 必须保证axiosAutoRetry方法，在axios添加拦截器之前执行，保证拦截器添加在第一个
 
-            let retry = false;
-            let currentRetryCount = retryState?.count || 0;
-            if ( currentRetryCount < retryCount) {
-                retry = retryCondition(response);
-            }
-            let delay = 0;
-            if (retryDelay) {
-                delay = retryDelay(currentRetryCount + 1);
-            }
-            if (retry) {
-                return sleep(delay).then(() => {
-                    return instance(response.config);
-                });
-            }
-            return response;
-        },
-        rejected: (err: any) => {
-            throw err
-        },
+    // (instance.interceptors.response as any).handlers.unshift  这个方法可以添加到第一个，但不是axios暴露出来的方法
+
+    instance.interceptors.response.use((response: AxiosResponse) => {
+        const config = Object.assign(retryConfig, response.config.retryConfig);
+        const retryState = response.config.retryState;
+        const retry = whetherRetry(config, retryState, response);
+        if (retry) {
+            retryState.count += 1;
+            const delay = getDelay(config, retryState);
+            return sleep(delay).then(() => {
+                return instance(response.config);
+            });
+        }
+        return response;
+    }, (error: any) => {
+        Promise.reject(error);
     });
 }
 export default axiosAutoRetry;
--- src/type.ts
+++ src/type.ts
@@ -1,11 +1,22 @@
 import { AxiosResponse } from 'axios';
 
 export type RetryConfig = {
+    // 最多重试次数
     retryCount: number;
+    // 重试条件
     retryCondition: (response: AxiosResponse)=> boolean;
+    // 重试延迟时间 ms
     retryDelay: (count: number) => number;
 }
 
 export type RetryState = {
     count: number,
+}
+
+
+declare module 'axios' {
+    export interface AxiosRequestConfig {
+        retryConfig: RetryConfig,
+        retryState: RetryState
+    }
 }
